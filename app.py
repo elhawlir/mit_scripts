c10398c50f79668c9fc8b7c445945a7ac96af88f
Rashid Elhawli <33712172+elhawlir@users.noreply.github.com>
2021-07-01 13:41:45 +1000

    undo

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import streamlit as st
 import datetime
 from datetime import datetime as dt
 import os
-import streamlit_analytics as sta
 
 # authorising sheets api and opening registration form
 sheets_client = authorisation(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
@@ -64,8 +63,6 @@ exc_students = [i for i in total_numbers if i not in recipients] # members that 
 ###################################################################
 # streamlit output of community metrics
 
-sta.start_tracking()
-
 title = st.title('Muslims in Tech Community Breakdown')
 
 # get the current date and time
@@ -114,5 +111,3 @@ def state_search(state, state_df):
     return f'{len(result)} members in {state}'
 
 st.write(state_search(drop_down, search_df))
-
-sta.stop_tracking(save_to_json='outputs/analytics.json')
